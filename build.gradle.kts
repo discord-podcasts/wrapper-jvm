0f3ec2ce48561056c8f8ebc46314310d5453dbe4
Marian <elias-marian@web.de>
2023-04-20 18:42:31 +0200

    :heavy_minus_sign: Removed private dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,12 +13,6 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven(url = "https://systems.myra.bot/releases/") {
-        credentials {
-            username = property("REPO_NAME")?.toString()
-            password = property("REPO_SECRET")?.toString()
-        }
-    }
 }
 
 dependencies {
@@ -29,7 +23,6 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerialization") // Json serialization
     implementation("com.codahale:xsalsa20poly1305:0.11.0") // Encryption / Decryption
 
-    testImplementation("bot.myra", "Diskord", "2.81")
     testImplementation(kotlin("reflect"))
     testImplementation("org.reflections:reflections:0.10.2")
 }
